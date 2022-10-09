87dc8a2a400d0c0fadf098c95170430662017384
chrystalme <afam_ifeanyi@live.com>
2022-10-09 21:29:05 +0300

    Add style for header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import { FaRegUserCircle } from 'react-icons/fa';
 import style from './Header.module.css';
 export const Header = () => {
   return (
-    <div className={style.mainHeader}>
-      <div>TravelAccess</div>
-      <div className={style.left}>
-        <div>My Trip </div>
-        <div>
-          <FaRegUserCircle />
+    <div className={style.wrapper}>
+      <div className={style.mainHeader}>
+        <div>TravelAccess</div>
+        <div className={style.left}>
+          <div>My Trip </div>
+          <div>
+            <FaRegUserCircle />
+          </div>
         </div>
       </div>
     </div>
