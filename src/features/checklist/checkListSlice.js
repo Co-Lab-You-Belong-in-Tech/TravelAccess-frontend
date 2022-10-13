412df8b18d000ce30c67ff4a823c8c7211c2cd36
chrystalme <afam_ifeanyi@live.com>
2022-10-13 10:20:43 +0300

    Fix redux store

--- src/features/checklist/checkListSlice.js
+++ src/features/checklist/checkListSlice.js
@@ -1,28 +1,28 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// export const checkListSlice = createSlice({
-//   name: 'checklist',
+export const checkListSlice = createSlice({
+  name: 'checklist',
 
-//   initialState: {
-//     checklist: [],
-//   },
-//   reducers: {
-//     addItem: (state, actions) => {
-//       // Redux Toolkit allows us to write "mutating" logic in reducers. It
-//       // doesn't actually mutate the state because it uses the Immer library,
-//       // which detects changes to a "draft state" and produces a brand new
-//       // immutable state based off those changes
-//       state.checklist.push(actions.payload);
-//     },
-//     removeItem: (state, actions) => {
-//       state.checklist = state.checklist.filter(
-//         item => item.id !== actions.payload.id
-//       );
-//     },
-//   },
-// });
+  initialState: {
+    checklist: [],
+  },
+  reducers: {
+    addItem: (state, actions) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.checklist.push(actions.payload);
+    },
+    removeItem: (state, actions) => {
+      state.checklist = state.checklist.filter(
+        item => item.id !== actions.payload.id
+      );
+    },
+  },
+});
 
-// // Action creators are generated for each case reducer function
-// export const { addItem, removeItem } = checkListSlice.actions;
+// Action creators are generated for each case reducer function
+export const { addItem, removeItem } = checkListSlice.actions;
 
-// export default checkListSlice.reducer;
+export default checkListSlice.reducer;
