623d8a6cc0363ac3901bc64bc05bdb632bf7b5da
Ijeoma Igboagu <ijeonyi@gmail.com>
2022-10-14 22:14:03 +0100

    update

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -10,147 +10,151 @@ import { MdLocationOn } from 'react-icons/md';
 
 const Homepage = () => {
   return (
-    <div>
-      <Header />
-      <main className={style.containerHomepage}>
-        <div className={style.homepageInnerTabs}>
-          <ul className={style.homepageTabs}>
-            <li className={style.links}>Flights</li>
-            <li className={style.links}>Stay</li>
-            <li className={style.links}>Bundles</li>
-          </ul>
-        </div>
-        <section className=' p-1 lg:p-12'>
-          <div className='flex mb-2 '>
-            <h4>One Way</h4>
-            <h4 className='font-bold ml-4'>Roundtrip</h4>
-          </div>
-          <div>
-            <form>
-              <div className={style.formUp}>
-                <div className='flex flex-col location'>
-                  <label
-                    htmlFor='input-group-1'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    From
-                  </label>
-                  <div className='relative mb-6'>
-                    <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                      <MdLocationOn />
-                    </div>
-                    <input
-                      type='text'
-                      id='input-group-1'
-                      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5'
-                      placeholder='Enter a city or airport'
-                    />
-                  </div>
-                </div>
+		<div>
+			<Header />
+			<main className={style.containerHomepage}>
+				<div className={style.homepageInnerTabs}>
+					<ul className={style.homepageTabs}>
+						<li className={style.links}>Flights</li>
+						<li className={style.links}>Stay</li>
+						<li className={style.links}>Bundles</li>
+					</ul>
+				</div>
+				<section className=' p-1 lg:p-6'>
+					<div className='flex mb-2 '>
+						<h4>One Way</h4>
+						<h4 className='font-bold ml-4'>Roundtrip</h4>
+					</div>
+					<div>
+						<form>
+							<div className={style.formUp}>
+								<div className='flex flex-col location'>
+									<label
+										htmlFor='input-group-1'
+										className='block mb-2 text-sm font-medium text-gray-900'>
+										From
+									</label>
+									<div className='relative mb-6'>
+										<div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+											<MdLocationOn />
+										</div>
+										<input
+											type='text'
+											id='input-group-1'
+											className={style.homepageInput}
+											placeholder='Enter a city or airport'
+										/>
+									</div>
+								</div>
 
-                <div className='flex flex-col'>
-                  <label
-                    htmlFor='input-group-1'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    To
-                  </label>
-                  <div className='relative mb-6'>
-                    <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                      <MdLocationOn />
-                    </div>
-                    <input
-                      type='text'
-                      id='input-group-1'
-                      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5'
-                      placeholder='Enter your destination'
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className={style.formDown}>
-                {/* <div date-rangepicker className='flex flex-col'> */}
-                <div className='flex flex-col'>
-                  <label className='mb-2'> Departing</label>
-                  <input
-                    name='start'
-                    type='date'
-                    className='p-1'
-                    placeholder='select date'
-                  />
-                </div>
-                <div className='flex flex-col'>
-                  <label className='mb-2'>Returning</label>
-                  <input
-                    name='end'
-                    type='date'
-                    className='p-1'
-                    placeholder='select date'
-                  />
-                  {/* </div> */}
-                </div>
-                <div className='flex flex-col'>
-                  <label className='mb-2'>Travelers</label>
-                  <select
-                    className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5'
-                    name='travelers'
-                    id='travelers'
-                  >
-                    <option value='1'>1 Adult</option>
-                    <option value='2'>2</option>
-                    <option value='3'>3</option>
-                    <option value='4'>4</option>
-                    <option value='5'>5</option>
-                  </select>
-                </div>
-                <div className='flex flex-col'>
-                  <label className='mb-2'>Tickets</label>
-                  <select
-                    className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5'
-                    name='travelers'
-                    id='travelers'
-                  >
-                    <option value='1'>First Class</option>
-                    <option value='2'>Business</option>
-                    <option value='3'>Economy</option>
-                  </select>
-                </div>
-              </div>
-              <div className={style.buttonContainer}>
-                <button
-                  type='submit'
-                  className='rounded-full border-black border p-1 w-44  bg-purple-800 text-white border-none'
-                >
-                  Search
-                </button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
-      <div className=' border-gray-400 border-b-2 mt-2 '></div>
-      <section className='container mx-auto p-1'>
-        <div className={style.explore}>
-          <h4>Explore Destination</h4>
-          <h4>view all</h4>
-        </div>
-        <div className='grid gap-3 grid-cols-2'>
-          <div>
-            <img src={img1} alt='flight' className='h-64 w-full object-cover' />
-          </div>
-          <div>
-            <img src={img2} alt='flight' className='h-64 w-full object-cover' />
-          </div>
-        </div>
-        <div className='grid grid-cols-1'>
-          <div className='mt-6'>
-            <img src={img3} alt='flight' className='w-full h-64 object-cover' />
-          </div>
-        </div>
-      </section>
-      <Footer />
-    </div>
-  );
+								<div className='flex flex-col'>
+									<label
+										htmlFor='input-group-1'
+										className='block mb-2 text-sm font-medium text-gray-900'>
+										To
+									</label>
+									<div className='relative mb-6'>
+										<div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+											<MdLocationOn />
+										</div>
+										<input
+											type='text'
+											id='input-group-1'
+											className={style.homepageInput}
+											placeholder='Enter your destination'
+										/>
+									</div>
+								</div>
+							</div>
+							<div className={style.formDown}>
+								{/* <div date-rangepicker className='flex flex-col'> */}
+								<div className='flex flex-col'>
+									<label className='mb-2'> Departing</label>
+									<input
+										name='start'
+										type='date'
+										className={style.nextInput}
+										placeholder='select date'
+									/>
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Returning</label>
+									<input
+										name='end'
+										type='date'
+										className={style.nextInput}
+										placeholder='select date'
+									/>
+									{/* </div> */}
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Travelers</label>
+									<select
+										className={style.nextInput}
+										name='travelers'
+										id='travelers'>
+										<option value='1'>1 Adult</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
+									</select>
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Tickets</label>
+									<select
+										className={style.nextInput}
+										name='travelers'
+										id='travelers'>
+										<option value='1'>Economy</option>
+										<option value='2'>Business</option>
+										<option value='3'>First class</option>
+									</select>
+								</div>
+							</div>
+							<div className={style.buttonContainer}>
+								<button
+									type='submit'
+									className='rounded-full border-black border p-1 w-44'>
+									Search
+								</button>
+							</div>
+						</form>
+					</div>
+				</section>
+			</main>
+			<div className=' border-gray-400 border-b-2 mt-2 '></div>
+			<section className='container mx-auto p-1'>
+				<div className={style.explore}>
+					<h4>Explore Destination</h4>
+					<h4>view all</h4>
+				</div>
+				<div className='grid gap-1 grid-cols-2'>
+					<div>
+						<img
+							src='https://via.placeholder.com/541x167'
+							alt='placeholder picture 1'
+						/>
+					</div>
+					<div>
+						<img
+							src='https://via.placeholder.com/541x167'
+							alt='placeholder picture 1'
+						/>
+					</div>
+				</div>
+				<div className='grid grid-cols-1'>
+					<div className='mt-6 flex justify-center items-center'>
+						<img
+							src='https://via.placeholder.com/924x240'
+							alt='placeholder picture 1'
+						/>
+					</div>
+				</div>
+			</section>
+			<Footer />
+		</div>
+	);
 };
 
 export default Homepage;
--- src/components/Trip/index.jsx
+++ src/components/Trip/index.jsx
@@ -1,121 +1,155 @@
 import style from './Trip.module.css';
 import { BsArrowLeftRight } from 'react-icons/bs';
+import { MdLocationOn } from 'react-icons/md';
 
 export const Trip = () => {
   return (
-    <div className={style.outerBox}>
-      <div className={style.innerBox}>
-        <div className={style.outerTabs}>
-          <div className={style.innerTabs}>
-            <div className={style.tabs}>
-              <h4>Flight</h4>
-              <h4>Stay</h4>
-              <h4>Bundles</h4>
-            </div>
-          </div>
-        </div>
-        <form className='w-[85%] m-auto'>
-          <div className='flex flex-row justify-between '>
-            <div className='flex flex-row justify-start items-center '>
-              <div className={style.formItem}>
-                <label htmlFor='Departing'>Departing</label>
-                <input
-                  type='text'
-                  name='departing'
-                  // onChange={e => setDeparting(e.target.value)}
-                />
-              </div>
-              <div className=''>
-                <button className='relative'>
-                  <BsArrowLeftRight />
-                </button>
-              </div>
-              <div className={style.formItem}>
-                <label htmlFor='Returning'>Returning</label>
-                <input type='text' name='returning' />
-              </div>
-            </div>
-            <div className='flex justify-end'>
-              <div className={style.formItem}>
-                <label htmlFor='traveller'>Traveller</label>
-                <input type='number' name='traveller' />
-              </div>
-            </div>
-          </div>
-          <div className='flex flex-row justify-between'>
-            <div className='flex flex-row justify-around w-full'>
-              <div className={style.formItem}>
-                <label htmlFor='from'>From</label>
-                <input type='date' name='from' da />
-              </div>
-              <div className={style.formItem}>
-                <label htmlFor='to'>To</label>
-                <input type='date' name='to' />
-              </div>
-            </div>
-            <div className='flex justify-end'>
-              <div className={style.formItem}>
-                <label htmlFor='ticket'>Ticket</label>
-                <input type='number' name='ticket' />
-                {/* <select className='w-full' id='ticket' name='ticket'>
-                  <option value='oneWay'>One Way</option>
-                </select> */}
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div>
-        <div>
-          <h5 className='text-gray-900 text-lg leading-tight font-medium mb-2'>
-            Upcoming
-          </h5>
-          <div className='block p-6 rounded-lg shadow-lg bg-white max-w-sm'>
-            <p className='text-gray-700 text-base mb-4'>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h3>Discover</h3>
-        <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-2 justify-center'>
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-          <img
-            src='https://via.placeholder.com/350x150'
-            alt='placeholder picture 1'
-          />
-        </div>
-        <div className='flex flex-row justify-around'></div>
-      </div>
-    </div>
-  );
+		<div className={style.outerBox}>
+			<div className={style.innerBox}>
+				<div className={style.outerTabs}>
+					<div className={style.innerTabs}>
+						<div className={style.tabs}>
+							<h4>Flight</h4>
+							<h4>Stay</h4>
+							<h4>Bundles</h4>
+						</div>
+					</div>
+				</div>
+				<section className='p-1 lg:p-6'>
+					<div className='flex'>
+						<h4>One Way</h4>
+						<h4 className='font-bold ml-4'>Roundtrip</h4>
+					</div>
+					<div>
+						<form>
+							<div className={style.formUp}>
+								<div className='flex flex-col location'>
+									<label
+										htmlFor='input-group-1'
+										className='block mb-2 text-sm font-medium text-gray-900'>
+										From
+									</label>
+									<div className='relative mb-6'>
+										<div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+											<MdLocationOn />
+										</div>
+										<input
+											type='text'
+											id='input-group-1'
+											className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5'
+											placeholder='Enter a city or airport'
+										/>
+									</div>
+								</div>
+
+								<div className='flex flex-col'>
+									<label
+										htmlFor='input-group-1'
+										className='block mb-2 text-sm font-medium text-gray-900'>
+										To
+									</label>
+									<div className='relative mb-6'>
+										<div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+											<MdLocationOn />
+										</div>
+										<input
+											type='text'
+											id='input-group-1'
+											className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5'
+											placeholder='Enter your destination'
+										/>
+									</div>
+								</div>
+							</div>
+							<div className={style.formDown}>
+								{/* <div date-rangepicker className='flex flex-col'> */}
+								<div className='flex flex-col'>
+									<label className='mb-2'> Departing</label>
+									<input
+										name='start'
+										type='date'
+										className='p-1'
+										placeholder='select date'
+									/>
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Returning</label>
+									<input
+										name='end'
+										type='date'
+										className='p-1'
+										placeholder='select date'
+									/>
+									{/* </div> */}
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Travelers</label>
+									<select
+										className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5'
+										name='travelers'
+										id='travelers'>
+										<option value='1'>1 Adult</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
+									</select>
+								</div>
+								<div className='flex flex-col'>
+									<label className='mb-2'>Tickets</label>
+									<select
+										className=' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5'
+										name='travelers'
+										id='travelers'>
+										<option value='1'>Economy</option>
+										<option value='2'>Business</option>
+										<option value='3'>First class</option>
+									</select>
+								</div>
+							</div>
+							<div className={style.buttonContainer}>
+								<button
+									type='submit'
+									className='rounded-full border-black border p-1 w-44  bg-purple-800 text-white border-none'>
+									Search
+								</button>
+							</div>
+						</form>
+					</div>
+				</section>
+			</div>
+			<div>
+				<div>
+					<h5 className='text-gray-900 text-lg leading-tight font-medium mb-2'>
+						Upcoming
+					</h5>
+					<img
+						src='https://via.placeholder.com/544x228'
+						alt='placeholder picture 1'
+					/>
+				</div>
+			</div>
+			<div>
+				<div className='flex justify-between items-center text-lg text-gray-900 font-medium mb-2 '>
+					<h3>Discover</h3>
+					<h3>View All</h3>
+				</div>
+				<div className='grid grid-cols-1 lg:grid-cols-3 gap-2 justify-center'>
+					<img
+						src='https://via.placeholder.com/352x162'
+						alt='placeholder picture 1'
+					/>
+					<img
+						src='https://via.placeholder.com/352x162'
+						alt='placeholder picture 1'
+					/>
+					<img
+						src='https://via.placeholder.com/352x162'
+						alt='placeholder picture 1'
+					/>
+				</div>
+				<div className='flex flex-row justify-around'></div>
+			</div>
+		</div>
+	);
 };
