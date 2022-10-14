81fd3dca7974a4d0b1aa3cd27457503d48ea211b
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2022-10-14 13:15:25 +0300

    Add auth to store

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { Trip } from './components/Trip';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/dashboard' element={<Dashboard />}>
+        <Route path='/dashboard/trip' element={<Trip />} />
+      </Route>
     </Routes>
   );
 };
