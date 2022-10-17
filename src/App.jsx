acec93a02237907c505472c3a7170dfe0c2bbf16
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2022-10-17 11:12:35 +0300

    Update SignUp component import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
-import Signup from './components/Signup';
+import Signup from './components/SignUp';
 import { TravelPrep } from './components/TravelPrep';
 import { Trip } from './components/Trip';
 
