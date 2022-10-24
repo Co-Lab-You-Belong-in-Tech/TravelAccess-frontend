52ffe3daeaddf2580a1e212d4487e2e6bc106a46
Ijeoma Igboagu <ijeonyi@gmail.com>
2022-10-24 13:00:55 +0100

    adding designs

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -14,48 +14,60 @@ const Login = () => {
   };
 
   return (
-    <div className={style.wrapper}>
-      <section className={style.loginContainer}>
-        <div className='bg-white lg:w-4/6 mt-12'>
-          <form
-            onSubmit={handleSubmit}
-            className='flex flex-col  p-10 justify-center items-center  '
-          >
-            <h2 className='mb-3 font-bold lg:text-3xl'>Welcome</h2>
+    <section className={style.baseView}>
+      <div className='bg-gray-200 w-1/2 h-1/2'>
+        <form
+          onSubmit={handleSubmit}
+          className='flex flex-col items-center  p-7'
+        >
+          <h2 className='mb-3 font-bold lg:text-3xl'>Welcome</h2>
+          <div class='relative'>
             <input
-              type='email'
-              placeholder='janedoe@gmail.com'
-              className='mb-7 p-3 border border-green-400 w-full'
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              required
+              type='text'
+              id='floating_filled'
+              class='block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+              placeholder=' '
             />
+            <label
+              for='floating_filled'
+              class='absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4'
+            >
+              Floating filled
+            </label>
+          </div>
+          {/* <input
+            type='email'
+            placeholder='janedoe@gmail.com'
+            className='mb-7 p-3'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+*/}
+          <div className=' flex justify-center items-center mt-8 '>
+            <Link to='homepage'>
+              <button
+                type='submit'
+                className=' rounded-full border-black border p-1 w-48 font-bold'
+              >
+                Login
+              </button>
+            </Link>
+          </div>
 
-            <div className=' flex justify-center items-center mt-8 '>
-              <Link to='homepage'>
-                <button
-                  type='submit'
-                  className=' rounded-full border-black border p-1 w-48 font-bold'
-                >
-                  Login
-                </button>
+          <div>
+            <p className='mt-8'>
+              Don't have an account?{' '}
+              <Link to='signup' className='font-bold'>
+                Signup
               </Link>
-            </div>
-
-            <div>
-              <p className='mt-8'>
-                Don't have an account?{' '}
-                <Link to='signup' className='font-bold'>
-                  Signup
-                </Link>
-              </p>
-            </div>
-          </form>
-          
-        </div>
-      </section>
-    </div>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
 export default Login;
+
--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -2,80 +2,76 @@ import { FaRegUserCircle } from 'react-icons/fa';
 import img from '../../assets/signupimg.jpg';
 import { useState } from 'react';
 import style from './Signup.module.css';
+import baseStyle from '../Login/Login.module.css';
 
 const Signup = () => {
-  const [inputs, setInputs] = useState({});
+	const [inputs, setInputs] = useState({});
 
-  const handleChange = event => {
-    const { name, value } = event.target;
-    setInputs(values => ({ ...values, [name]: value }));
-  };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setInputs((values) => ({ ...values, [name]: value }));
+	};
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(inputs);
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(inputs);
+	};
 
-  return (
-    <section className='container mx-auto mt-16 '>
-      <div className='bg-gray-200 lg:grid grid-cols-2'>
-        <form
-          onSubmit={handleSubmit}
-          className='flex flex-col  p-5 text-center '
-        >
-          <h2 className='mb-3 font-bold lg:text-3xl'>Welcome</h2>
-          <input
-            type='text'
-            name='fullname'
-            placeholder='Jane Doe'
-            className='mb-5 p-2'
-            value={inputs.fullname || ''}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type='email'
-            name='email'
-            placeholder='janedoe@gmail.com'
-            className='mb-5 p-2'
-            value={inputs.email || ''}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type='tel'
-            name='telephone'
-            placeholder='000-000-000'
-            className='mb-5 p-2'
-            value={inputs.telephone || ''}
-            onChange={handleChange}
-            required
-          />
-          <div className=' flex justify-center items-center mt-8 '>
-            <div>
-              <button
-                type='submit'
-                className=' rounded-full border-black border p-1 w-48 font-bold '
-              >
-                signup
-              </button>
-            </div>
-          </div>
-          <div>
-            <p className='mt-8'>
-              Have an account?{' '}
-              <a href='/' className='font-bold'>
-                Login
-              </a>
-            </p>
-          </div>
-        </form>
-        <div className='signupImg'>
-          <img src={img} alt='img' className={style.height} />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={baseStyle.baseView}>
+			<div className='bg-gray-200 w-1/2'>
+				<form
+					onSubmit={handleSubmit}
+					className='flex flex-col  p-5 text-center '>
+					<h2 className='mb-3 font-bold lg:text-3xl'>Welcome</h2>
+					<input
+						type='text'
+						name='fullname'
+						placeholder='Jane Doe'
+						className='mb-5 p-2'
+						value={inputs.fullname || ''}
+						onChange={handleChange}
+						required
+					/>
+					<input
+						type='email'
+						name='email'
+						placeholder='janedoe@gmail.com'
+						className='mb-5 p-2'
+						value={inputs.email || ''}
+						onChange={handleChange}
+						required
+					/>
+					<input
+						type='tel'
+						name='telephone'
+						placeholder='000-000-000'
+						className='mb-5 p-2'
+						value={inputs.telephone || ''}
+						onChange={handleChange}
+						required
+					/>
+					<div className=' flex justify-center items-center mt-8 '>
+						<div>
+							<button
+								type='submit'
+								className=' rounded-full border-black border p-1 w-48 font-bold '>
+								signup
+							</button>
+						</div>
+					</div>
+					<div>
+						<p className='mt-8'>
+							Have an account?{' '}
+							<a href='/' className='font-bold'>
+								Login
+							</a>
+						</p>
+					</div>
+				</form>
+			</div>
+		</section>
+	);
 };
 
 export default Signup;
