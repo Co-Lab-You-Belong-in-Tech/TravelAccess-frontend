4a9b441157cdab6d158c8041a075239c91d3d4bc
Ijeoma Igboagu <ijeonyi@gmail.com>
2022-11-09 21:17:08 +0100

    Added footer component to all pages.... added images left in travel prep also dropdown

--- src/components/Trip/index.jsx
+++ src/components/Trip/index.jsx
@@ -2,58 +2,61 @@ import style from './Trip.module.css';
 import { MdLocationOn } from 'react-icons/md';
 import TripUpcoming from '../TripUpcoming';
 import TripForm from '../TripForm';
+import Footer from '../Footer/index'
 
 export const Trip = () => {
   return (
-    <>
-      <TripForm />
+		<>
+			<TripForm />
 
-      <div className=' border-gray-200 border-b mt-2 '></div>
-      <div className={style.upcomingDashboard}>
-        <div className='text-primary mb-4 flex justify-between items-center'>
-          <h4 className='font-bold'>Upcoming</h4>
-          <h4 className={style.upcomimgText}>View All</h4>
-        </div>
-        <div>
-          <TripUpcoming />
-        </div>
+			<div className=' border-gray-200 border-b mt-2 '></div>
+			<div className={style.upcomingDashboard}>
+				<div className='text-primary mb-4 flex justify-between items-center'>
+					<h4 className='font-bold'>Upcoming</h4>
+					<h4 className={style.upcomimgText}>View All</h4>
+				</div>
+				<div>
+					<TripUpcoming />
+				</div>
 
-        <section className='mt-6'>
-          <div className='text-primary mb-4 flex justify-between items-center'>
-            <h4 className='font-bold'>Discover</h4>
-            <h4>View All</h4>
-          </div>
-        </section>
-        <div className='grid grid-cols-2 lg:grid-cols-3 gap-2'>
-          <div className='flex flex-col justify-start items-start '>
-            <img
-              src='../../assets/Images/Namsan tower discover 1x.png'
-              alt='Namsan Tower'
-              className='lg:h-52'
-            />
-            <p className='-mt-2 ml-2 text'>Namsan Tower</p>
-          </div>
-          <div>
-            <img
-              src='../../assets/Images/Bukchon Hanok Village discover 1x.png'
-              alt='Bukchon Hanok Village'
-              className='lg:h-52'
-            />
-            <p className='-mt-2 ml-2 text'>Bukchon Hanok Village</p>
-          </div>
-          <div className={style.bottomImg}>
-            <div className='flex flex-col '>
-              <img
-                src='../../assets/Images/Lotte World discover 1x.png'
-                alt='Lotte World'
-                className='lg:h-52'
-              />
-              <p className='-mt-2 ml-2 text'>Lotte World</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div></div>
-    </>
-  );
+				<section className='mt-6'>
+					<div className='text-primary mb-4 flex justify-between items-center'>
+						<h4 className='font-bold'>Discover</h4>
+						<h4>View All</h4>
+					</div>
+				</section>
+				<div className='grid grid-cols-2 lg:grid-cols-3 gap-2'>
+					<div className='flex flex-col justify-start items-start '>
+						<img
+							src='../../assets/Images/Namsan tower discover 1x.png'
+							alt='Namsan Tower'
+							className='lg:h-52'
+						/>
+						<p className='-mt-2 ml-2 text'>Namsan Tower</p>
+					</div>
+					<div>
+						<img
+							src='../../assets/Images/Bukchon Hanok Village discover 1x.png'
+							alt='Bukchon Hanok Village'
+							className='lg:h-52'
+						/>
+						<p className='-mt-2 ml-2 text'>Bukchon Hanok Village</p>
+					</div>
+					<div className={style.bottomImg}>
+						<div className='flex flex-col '>
+							<img
+								src='../../assets/Images/Lotte World discover 1x.png'
+								alt='Lotte World'
+								className='lg:h-52'
+							/>
+							<p className='-mt-2 ml-2 text'>Lotte World</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+        <Footer />
+			</div>
+		</>
+	);
 };
--- src/components/TripUpcoming/index.jsx
+++ src/components/TripUpcoming/index.jsx
@@ -108,6 +108,7 @@ const TripUpcoming = () => {
 				</div>
 			</div>
 			
+			
 		</>
 	);
 };
