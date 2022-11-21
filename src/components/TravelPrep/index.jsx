e66d2f5dc89eb26c8554880e2f9511ab8cf62d7e
Ijeoma Igboagu <ijeonyi@gmail.com>
2022-11-21 13:03:46 +0100

    changed color button

--- src/components/TravelPrep/index.jsx
+++ src/components/TravelPrep/index.jsx
@@ -271,7 +271,7 @@ export const TravelPrep = () => {
                           Close
                         </button>
                         <button
-                          className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                          className='bg-primary text-white active:bg-primary font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                           type='button'
                           onClick={() => setShowModal(false)}
                         >
